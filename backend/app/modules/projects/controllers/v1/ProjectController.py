3220ad8c990b523a28e5aacdd682ddd9914e8eee
bassem samaali <83089177+samaalibassem123@users.noreply.github.com>
2026-02-22 17:58:46 +0100

    add assign project

--- backend/app/modules/projects/controllers/v1/ProjectController.py
+++ backend/app/modules/projects/controllers/v1/ProjectController.py
@@ -10,3 +10,7 @@ async def get_projects(db:DB_dependecy, limit: int = 50,offset: int = 0):
     projects = await ProjectsService.get_projects(db,limit,offset)
     return projects
 
+@projects_router.post('/assign')
+async def assign_project(db:DB_dependecy, project_id:int ,users_id:list[str]):
+    res = await ProjectsService.assign_project(db, project_id, users_id)
+    return res
--- backend/app/modules/projects/services/ProjectsService.py
+++ backend/app/modules/projects/services/ProjectsService.py
@@ -2,7 +2,7 @@ from fastapi import HTTPException
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from app.core.database.models import EmployeeAttendanceEvent, AttendanceEvent, Project
+from app.core.database.models import EmployeeAttendanceEvent, AttendanceEvent, Project, UserProject
 
 
 class ProjectsService:
@@ -11,5 +11,21 @@ class ProjectsService:
         try:
             projects = await db.execute(select(Project).limit(limit).offset(offset))
             return projects.scalars().all()
+        except Exception as e:
+            raise HTTPException(status_code=400, detail=str(e))
+
+    @staticmethod
+    async def assign_project(db:AsyncSession, project_id, users_id:list[str]):
+        try:
+            projectUsers = []
+            for user_id in users_id:
+                projectUser = {
+                    "user_id":user_id,
+                    "project_id":project_id
+                }
+                projectUsers.append(UserProject(**projectUser))
+            db.add_all(projectUsers)
+            await db.commit()
+            return True
         except Exception as e:
             raise HTTPException(status_code=400, detail=str(e))
