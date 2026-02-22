aabfe2ebd6c93cf3a19725046dd903d17dc449c2
Bacem Samaali <Bacem.Samaali@onetech-group.corp>
2026-02-22 22:54:06 +0100

    correct filtring on chekcinou

--- backend/app/modules/chekinout/controllers/v1/CheckinoutController.py
+++ backend/app/modules/chekinout/controllers/v1/CheckinoutController.py
@@ -1,3 +1,5 @@
+import datetime
+
 from fastapi import APIRouter
 
 from app.core import DB_dependecy
@@ -7,6 +9,6 @@ checkinout_router = APIRouter(prefix='/v1/checkinout', tags=['Check in - Check o
 
 
 @checkinout_router.get('/')
-async def get_checkinouts(db:DB_dependecy, limit: int = 50,offset: int = 0, fullname:str | None = None, email: str | None = None, att_date:str|None=None):
-    checkinouts = await CheckinOutService.get_checkinouts(db, limit, offset ,fullname, email, att_date)
+async def get_checkinouts(db:DB_dependecy, limit: int = 50,offset: int = 0, fullname:str | None = None, email: str | None = None,  start_date:int|None=None, end_date:int|None=None):
+    checkinouts = await CheckinOutService.get_checkinouts(db, limit, offset ,fullname, email, start_date, end_date)
     return checkinouts
--- backend/app/modules/chekinout/services/CheckinoutService.py
+++ backend/app/modules/chekinout/services/CheckinoutService.py
@@ -1,5 +1,7 @@
+import datetime
+
 from fastapi import HTTPException
-from sqlalchemy import select, func
+from sqlalchemy import select, func, extract
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import joinedload
 
@@ -8,19 +10,24 @@ from app.core.database.models import Attendance, Employee
 
 class CheckinOutService:
     @staticmethod
-    async def get_checkinouts(db:AsyncSession, limit:int=50, offset:int=0, fullname:str | None = None, email: str | None = None, att_date:str|None=None):
+    async def get_checkinouts(db:AsyncSession, limit:int=50, offset:int=0, fullname:str | None = None, email: str | None = None,  start_date:int|None=None, end_date:int|None=None):
         try:
-            query = select(Attendance, Employee).join(Employee, Attendance.id == Employee.id )
+            query = select(Attendance, Employee).join(Employee, Attendance.emp_id == Employee.id )
 
             # filters
             if fullname :
                 query = query.where(Employee.full_name.ilike(f"%{fullname}%"))
             if email:
-                query = query.where(Employee.email.ilike(f"{email}"))
-            if att_date :
-                query = query.where(Attendance.att_date == att_date)
+                query = query.where(Employee.email.ilike(f"%{email}%"))
+            if start_date and end_date:
+                query = query.where(
+                    extract("year",Attendance.att_date) >= start_date,
+                    extract("year", Attendance.att_date) <= end_date
+                    )
+
             # total numbers of attendace
-            total = await db.execute(select(func.count()).select_from(query.subquery()))
+            total_res = await db.execute(select(func.count()).select_from(query.subquery()))
+            total = total_res.scalar_one()
             # result with pagination
             result = await db.execute(query.options(joinedload(Attendance.employee)).limit(limit).offset(offset))
             checkinouts = result.scalars().all()
