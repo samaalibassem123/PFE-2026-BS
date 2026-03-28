3790c86e490f0e5035f4705bf94d3f672147e286
bassem samaali <83089177+samaalibassem123@users.noreply.github.com>
2026-03-28 23:46:42 +0100

    update ui for sync and use v1 for loading

---
+++ backend/app/core/ETL/Loaders/v2/__init__.py
@@ -0,0 +1,8 @@
+'''
+
+
+this version is not completed
+
+it still have some errors and it need models updates
+
+'''
--- frontend/src/modules/settings/features/Synchronization/components/SyncronizeComponent.tsx
+++ frontend/src/modules/settings/features/Synchronization/components/SyncronizeComponent.tsx
@@ -16,72 +16,77 @@ import { cn } from "@/lib/utils";
 import { useSyncronization } from "../hooks/useSyncronization";
 
 export default function SyncronizeComponent() {
-const {steps, loading,Syncronize} = useSyncronization()
+  const { steps, loading, Syncronize } = useSyncronization();
 
-const [progress, setProgress] = useState(10);
+  const [progress, setProgress] = useState(0);
 
+  useEffect(() => {
+    if (steps) {
+      const timer = setTimeout(() => setProgress((100 * steps.length) / 9), 0);
+      return () => clearTimeout(timer);
+    }
+  }, [steps]);
 
+  return (
+    <>
+      <Card className=" w-full ">
+        <CardHeader>
+          <CardTitle className=" text-2xl">
+            System Data Synchronization (Biotime & EasyProject)
+          </CardTitle>
+          <CardDescription className="">
+            Synchronize and update all system data using ETL pipelines from
+            Biotime and EasyProject.
+          </CardDescription>
+        </CardHeader>
+        <CardContent className=" font-extralight text-[13px]">
+          <p className="mb-4">
+            <span className="font-bold ">Biotime : </span>
+            provides workforce and attendance data, including employee
+            check-ins, check-outs, working hours, shifts, and leave records. It
+            ensures accurate tracking of employee presence and time-related
+            information.
+          </p>
 
-useEffect(()=>{
-  console.log(steps)
-},[steps])
+          <p className="mb-4">
+            <span className="font-bold">EasyProject : </span>
+            contains project management data such as projects, tasks,
+            assignments, users, budgets, and progress tracking. It helps manage
+            operational workflows and project execution details.
+          </p>
 
-useEffect(() => {
-  const timer = setTimeout(() => setProgress(66), 500);
-  return () => clearTimeout(timer);
-}, []);
+          <p>
+            By combining both sources, this synchronization ensures that
+            employee activity and project information stay aligned and
+            consistently updated in your main database.
+          </p>
+        </CardContent>
 
-return (
-  <>
-    <Card className=" w-full ">
-      <CardHeader>
-        <CardTitle className=" text-2xl">
-          System Data Synchronization (Biotime & EasyProject)
-        </CardTitle>
-        <CardDescription className="">
-          Synchronize and update all system data using ETL pipelines from
-          Biotime and EasyProject.
-        </CardDescription>
-      </CardHeader>
-      <CardContent className=" font-extralight text-[13px]">
-        <p className="mb-4">
-          <span className="font-bold ">Biotime : </span>
-          provides workforce and attendance data, including employee check-ins,
-          check-outs, working hours, shifts, and leave records. It ensures
-          accurate tracking of employee presence and time-related information.
-        </p>
+        <CardFooter className=" justify-between gap-3">
+          {loading && (
+            <div className=" flex-1  gap-2 flex-col">
+              <div className=" flex mb-2 flex-col">
+                {steps &&
+                  steps?.length > 0 &&
+                  steps?.map((s, i) => (
+                    <Shimmer key={i}>{s.step + " : " + s.time + "s"}</Shimmer>
+                  ))}
+              </div>
 
-        <p className="mb-4">
-          <span className="font-bold">EasyProject : </span>
-          contains project management data such as projects, tasks, assignments,
-          users, budgets, and progress tracking. It helps manage operational
-          workflows and project execution details.
-        </p>
-
-        <p>
-          By combining both sources, this synchronization ensures that employee
-          activity and project information stay aligned and consistently updated
-          in your main database.
-        </p>
-      </CardContent>
-
-      <CardFooter className=" justify-between gap-3">
-        <div className=" flex-1 gap-2 flex-col">
-          <Shimmer>department....</Shimmer>
-          <Progress value={progress} />
-        </div>
-        <Button
-
-          size="lg"
-          variant={loading ? "secondary" : "default"}
-          disabled={loading}
-          onClick={() => Syncronize()}
-        >
-          <RefreshCcw className={cn(loading && " animate-spin")} />
-          Run Synchronization
-        </Button>
-      </CardFooter>
-    </Card>
-  </>
-);
+              <Progress value={progress} />
+            </div>
+          )}
+          <Button
+            size="lg"
+            variant={loading ? "secondary" : "default"}
+            disabled={loading}
+            onClick={() => Syncronize()}
+          >
+            <RefreshCcw className={cn(loading && " animate-spin")} />
+            Run Synchronization
+          </Button>
+        </CardFooter>
+      </Card>
+    </>
+  );
 }
